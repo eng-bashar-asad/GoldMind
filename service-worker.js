7ad05310fd01062d8b0a57bd8f18151b57a2eda5
eng-bashar-asad <eng.bashar.asad@gmail.com>
2026-08-04 03:14:03 +0400

    Add PWA service worker

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'goldmind-shell-v1';
+const SHELL_ASSETS = [
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(SHELL_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Network-first: always try the network (so data/pages stay fresh), fall back
+// to cache only if the network is unreachable (basic offline resilience).
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(() => {});
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
