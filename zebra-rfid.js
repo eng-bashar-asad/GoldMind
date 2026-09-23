59737f6805836c0564694d0278df612b40e02c32
eng-bashar-asad <eng.bashar.asad@gmail.com>
2026-09-23 18:49:00 +0400

    Render diamond_carat field as value+'ct' on printed labels

--- zebra-rfid.js
+++ zebra-rfid.js
@@ -171,6 +171,7 @@ var GMZebra = (function () {
         case 'karat': return 'K' + (values.karat || '');
         case 'mc': return 'MC:' + (values.mc != null ? values.mc : '');
         case 'description': return values.description || '';
+        case 'diamond_carat': return (values.diamondCarat != null && values.diamondCarat !== '') ? (values.diamondCarat + 'ct') : '';
         case 'custom_text': return field.custom_text || '';
         default: return '';
       }
